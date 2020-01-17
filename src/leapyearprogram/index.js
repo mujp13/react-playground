0fe33d0a57da59755665dbf287888841b3d41ecb
Kevin Park <keunsuk413@gmail.com>
2020-01-16 21:17:44 -0600

    unit test exercise

---
+++ src/leapyearprogram/index.js
@@ -0,0 +1,12 @@
+function leapYear(year) {
+  if(year < 1582) {
+    throw error("This is not leap year!");
+  }
+  const div4 = year % 4 === 0;
+  const div100 = year % 100 === 0;
+  const div400 = year % 400 === 0;
+
+  return div400 || (div4 && !div100);
+}
+
+export default leapYear;
