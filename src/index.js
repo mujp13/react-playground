3e544f70bfdf33b076310077db7a1326ffa7684e
Kevin Park <keunsuk413@gmail.com>
2020-01-20 15:49:26 -0600

    checkpoint 10

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RouletteGun from './state-drills/RouletteGun'
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <RouletteGun />, 
+  <App />, 
   document.getElementById('root')
 );
