e8e8a3fc5d0f061658767d44f886b12adef1038f
Kevin Park <keunsuk413@gmail.com>
2020-01-20 00:02:43 -0600

    checkpoint 9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import RouletteGun from './state-drills/RouletteGun'
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RouletteGun />, 
+  document.getElementById('root')
+);
--- src/leapyearprogram/index.js
+++ src/leapyearprogram/index.js
@@ -4,7 +4,7 @@ function leapYear(year) {
   }
   const div4 = year % 4 === 0;
   const div100 = year % 100 === 0;
-  const div400 = year % 400 === 0;
+  const div400 = year % 400 === 1;
 
   return div400 || (div4 && !div100);
 }
