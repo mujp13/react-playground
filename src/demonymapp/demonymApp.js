20ddc07a87cc1d10afe3444b941c8e205567a176
Kevin Park <keunsuk413@gmail.com>
2020-02-07 16:01:34 -0600

    added react router codes

--- src/demonymapp/demonymApp.js
+++ src/demonymapp/demonymApp.js
@@ -13,13 +13,28 @@ export default class DemonymApp extends React.Component {
 
   componentDidMount() {
     fetch('https://country.register.gov.uk/records.json?page-size=5000')
+      .then(response => {
+        // check if response is ok
+        console.log('About to check for errors');
+        if(!response.ok) {
+          throw new Error('Something went wrong'); // throw an error
+        }
+        return response; // ok, so just continue
+      })
       .then(response => response.json())
       .then(data => {
         const countries = Object.keys(data).map(key => data[key].item[0]);
         this.setState({
           countries
         })
-      });
+      })
+      .catch(err => {
+        // this catch handles the error condition
+        this.setState({
+          error: err.message
+        })
+        console.log('Handling the error here.', err);
+      })
   }
 
   setSelected(selected) {
@@ -33,10 +48,11 @@ export default class DemonymApp extends React.Component {
           ? <Demonym name={this.state.selected['citizen-names']} country={this.state.selected.name}/>
           : <div className="demonym_app__placeholder">Select a country above</div>;
     
-    //console.log(this.state.selected['citizen-names'])
-    //console.log(this.state.selected.name)
-
-    return (
+    const error = this.state.error
+          ? <div className="demonym_app__error">{this.state.error}</div>
+          : "";
+ 
+      return (
       <div className="demonym_app">
         <CountrySelector 
                 countries={this.state.countries}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
+import App from './paltrows-power-toes/App';
 
 ReactDOM.render(
-  <App />, 
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
