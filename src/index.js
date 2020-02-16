795d7b27bce36157761f28a11b1c1a53164c58d3
Kevin Park <keunsuk413@gmail.com>
2020-02-15 20:11:51 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './paltrows-power-toes/App';
+// import App from './paltrows-power-toes/App';
+import AppLang from './lang-context/AppLang';
 
 ReactDOM.render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>,
+  <AppLang />,
   document.getElementById('root')
 );
