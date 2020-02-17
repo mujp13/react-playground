72d3baf2c1db07ff436276ae1c6ef89bd864c123
Kevin Park <keunsuk413@gmail.com>
2020-02-17 17:39:22 -0600

    added context

--- src/lang-context/LangControls.js
+++ src/lang-context/LangControls.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import LanguageContext from './LanguageContext';
 
-console.log(LanguageContext);
-
 export default function LangControls(props) {
   return (
-    <>
-      <button>
-        British{' '}
-        <span role='img' aria-label='en-GB'>🇬🇧</span>
-      </button>
-      {' '}
-      <button>
-        American{' '}
-        <span role='img' aria-label='en-US'>🇺🇸</span>
-      </button>
-      {' '}
-      <button>
-        Korean{' '}
-        <span role='img' aria-label='ko'>🇰🇷</span>
-      </button>
-    </>
+    <LanguageContext.Consumer>
+      {(value) => {
+        return(
+          <>
+            <button
+              onClick={() => props.onSetLang('en-GB')}
+              disabled={value.lang === 'en-GB'}>
+              British{' '}
+              <span role='img' aria-label='en-GB'>🇬🇧</span>
+            </button>
+            {' '}
+            <button
+            onClick={() => props.onSetLang('en-US')}
+              disabled={value.lang === 'en-US'}>
+              American{' '}
+              <span role='img' aria-label='en-US'>🇺🇸</span>
+            </button>
+            {' '}
+            <button
+              onClick={() => props.onSetLang('ko')}
+              disabled={value.lang === 'ko'}>
+              Korean{' '}
+              <span role='img' aria-label='ko'>🇰🇷</span>
+            </button>
+          </>
+        )
+      }}
+    </LanguageContext.Consumer>
   );
 }
+
+
